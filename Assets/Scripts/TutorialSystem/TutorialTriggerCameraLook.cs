5e45ae9ec29c5a934461216a386ccb040125f577
obrda <73131312+obrda@users.noreply.github.com>
2020-12-16 19:24:09 +0300

    Tutorial System
    First three triggers

---
+++ Assets/Scripts/TutorialSystem/TutorialTriggerCameraLook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTriggerCameraLook : MonoBehaviour
+{
+    [SerializeField] private float range = 2.0f;
+    private Ray _ray;
+    private TutorialSystem system;
+
+    void Start()
+    {
+        system = transform.GetComponentInParent<TutorialSystem>();
+    }
+
+    void Update()
+    {
+        _ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        if (Physics.Raycast(_ray, out var hit, range, LayerMask.GetMask("Tutorial")))
+            system.Triggered();
+    }
+}
