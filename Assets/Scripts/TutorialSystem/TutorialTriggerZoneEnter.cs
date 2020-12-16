ecc70c8808f98f65cb2b93aeb90892290d73843a
obrda <73131312+obrda@users.noreply.github.com>
2020-12-16 19:24:09 +0300

    Tutorial System
    First three triggers

---
+++ Assets/Scripts/TutorialSystem/TutorialTriggerZoneEnter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTriggerZoneEnter : MonoBehaviour
+{
+    private TutorialSystem system;
+
+    void Start()
+    {
+        system = transform.GetComponentInParent<TutorialSystem>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+            system.Triggered();
+    }
+
+}
