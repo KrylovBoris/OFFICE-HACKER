ffbe7d83e156245b96866aad77d88bc8d9ae0100
Boris Krylov <hakkanx@gmail.com>
2021-03-01 22:58:45 +0300

    Some useful libs. Probably will delete later

--- Assets/NoteSpawner.cs
+++ Assets/NoteSpawner.cs
@@ -109,16 +109,4 @@ public class NoteSpawner : MonoBehaviour
     {
         SpawnNote(text, hiddenZoneIndex);
     }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
